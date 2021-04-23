413cfb95be62cdcd9fa1586f0c166b1900e46dad
triana <trianaheinz@gmail.com>
2021-04-23 13:39:28 +0200

    models done

--- models/Groups.model.js
+++ models/Groups.model.js
@@ -2,19 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-  image: { type: String, default: '/public/images/man.png' },
+  image: { type: String, default: '/public/images/climbing.jpeg' },
   name: { type: String, required: true },
-  description: { type: String, minlength: 100, maxlength: 200 },
-  city: { type: String },
-  sport: { type: ['String'] },
-  role: {
+  description: { type: String, minlength: 100, maxlength: 200, required: true },
+  announcement: {
     type: String,
-    enum: ['User', 'Admin', 'Editor'],
-    default: 'User',
+    minlength: 100,
+    maxlength: 200,
+    required: true,
   },
+  city: { type: String, required: true },
+  sport: { type: ['String'], required: true },
+  role: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 
-const User = mongoose.model('User', userSchema);
+const Group = mongoose.model('Group', groupSchema);
 
 module.exports = Group;
--- models/User.model.js
+++ models/User.model.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    image: { type: String, default: '/public/images/man.png' },
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String},
-    description: {type: String, minlength: 100, maxlength:200},
-    age: {type:Number},
-    city: {type: String},
-    sport: {type:["String"]},
-    role: {
-      type: String,
-      enum: ["User", "Admin", "Editor"],
-      default: "User"
+  image: { type: String, default: '/public/images/man.png' },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },
+  description: { type: String, minlength: 100, maxlength: 200 },
+  age: { type: Number },
+  city: { type: String },
+  sport: { type: ['String'] },
+  role: {
+    type: String,
+    enum: ['User', 'Admin'],
+    default: 'User',
   },
-    level: {enum:["Beginner", "Intermedio", "Avanzado"]},
-    CreateGroup: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-})
+  level: { enum: ['Beginner', 'Intermedio', 'Avanzado'] },
+  groups: [{ type: Schema.Types.ObjectId, ref: 'Group' }],
+});
 
 const User = mongoose.model('User',userSchema);
 

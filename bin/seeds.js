483f8307492613bae72691de2a8acd611ec039d7
triana <trianaheinz@gmail.com>
2021-04-26 16:45:30 +0200

    data base updated

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,33 +1,95 @@
-const users = require('../dataUsers'); //Requiriendo lo que hay en el fichero data.js
+require('dotenv').config();
 const User = require('../models/User.model');
-const groups = require('../dataGroups');
-const Groups = require ('../models/Groups.model')
+const Groups = require('../models/Groups.model');
 const mongoose = require('mongoose'); //Paquete para conectarme a la base de datos
 const DB_NAME = 'meet-train-app'; //nombre de la base de datos que se va a crear.
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
-mongoose.connect(`mongodb://localhost/${DB_NAME}`) //Nos conectamos a la base de datos y nos devuelve una promesa.
-    .then(() => { // Aquí estamos 100% seguros que estamos conectados a la base de datos.
-        console.log('Connected to database');
-        //Cojemos el modelo escalador
-        User.insertMany(users) //Insertar varios
-            .then(users => {
-                console.log(`${users.length} inserted`); //Cuantos escaladores tenemos en total.
-            })
-    });
-    .then
+let userIds = [];
 
-.catch(error => console.error(error));
 
 mongoose
   .connect(`mongodb://localhost/${DB_NAME}`) //Nos conectamos a la base de datos y nos devuelve una promesa.
-  .then(() => {
+  .then((db) => {
     // Aquí estamos 100% seguros que estamos conectados a la base de datos.
     console.log('Connected to database');
-    //Cojemos el modelo escalador
-    Groups.insertMany(groups) //Insertar varios
-      .then(groups => {
-        console.log(`${groups.length} inserted`); //Cuantos escaladores tenemos en total.
-      });
+    return db.connection.dropDatabase();
+  })
+  .then(() => {
+    const users = [
+      {
+        image:
+          'https://i.chzbgr.com/full/9151546112/hD5CC5989/el-fisiculturista-germanlifter-se-crea-un-perfil-falso-en-tinder',
+        username: 'Sam',
+        email: 'sam@hotmail.com',
+        password: '12345',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tellus in iaculis feugiat. Nullam sit amet massa metus. Duis in orci lectus. Praesent et erat nisl. Ut non eleifend odio. Fusce quis ultrices nisi. Suspendisse imperdiet lorem eu ligula finibus facilisis. Nullam sit amet orci viverra, iaculis ipsum sit amet, cursus nibh. Mauris semper, justo eget auctor vestibulum, sem erat bibendum turpis, at rhoncus mauris nisl vitae purus. Curabitur tempor mauris sit amet pulvinar faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi suscipit sed est ac finibus. Donec feugiat, felis eleifend congue posuere, ipsum libero fringilla ex, non blandit eros ipsum sit amet velit.',
+        age: 20,
+        city: 'Barcelona',
+        sport: 'escalada',
+        groups: 'escalada en barcelona',
+      },
+      {
+        image: '/public/images/man.png',
+        username: 'Adrian',
+        email: 'ad@hotmail.com',
+        password: '12345',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tellus in iaculis feugiat. Nullam sit amet massa metus. Duis in orci lectus. Praesent et erat nisl. Ut non eleifend odio. Fusce quis ultrices nisi. Suspendisse imperdiet lorem eu ligula finibus facilisis. Nullam sit amet orci viverra, iaculis ipsum sit amet, cursus nibh. Mauris semper, justo eget auctor vestibulum, sem erat bibendum turpis, at rhoncus mauris nisl vitae purus. Curabitur tempor mauris sit amet pulvinar faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi suscipit sed est ac finibus. Donec feugiat, felis eleifend congue posuere, ipsum libero fringilla ex, non blandit eros ipsum sit amet velit.',
+        age: 24,
+        city: 'Barcelona',
+        sport: 'surf',
+        groups: 'surf en barcelona',
+      },
+      {
+        image: '/public/images/man.png',
+        username: 'Triana',
+        email: 'tri@hotmail.com',
+        password: '12345',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tellus in iaculis feugiat. Nullam sit amet massa metus. Duis in orci lectus. Praesent et erat nisl. Ut non eleifend odio. Fusce quis ultrices nisi. Suspendisse imperdiet lorem eu ligula finibus facilisis. Nullam sit amet orci viverra, iaculis ipsum sit amet, cursus nibh. Mauris semper, justo eget auctor vestibulum, sem erat bibendum turpis, at rhoncus mauris nisl vitae purus. Curabitur tempor mauris sit amet pulvinar faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi suscipit sed est ac finibus. Donec feugiat, felis eleifend congue posuere, ipsum libero fringilla ex, non blandit eros ipsum sit amet velit.',
+        age: 30,
+        city: 'Barcelona',
+        sport: 'correr',
+        groups: 'correr en barcelona',
+      },
+    ];
+    users.forEach((user) => {
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(user.password, salt);
+      user.password = hashedPassword;
+    });
+    return User.create(users);
+  })
+  .then((createdUsers) => {
+    console.log(`Created ${createdUsers.length} users.`);
+    createdUsers.forEach((user) => {
+      userIds.push(user._id);
+    });
+    console.log(userIds);
+    return userIds;
+  })
+  .then((ids) => {
+    const groups = [
+      {
+        image: '/public/images/climbing.jpeg',
+        name: 'escalar en barcelona',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tellus in iaculis feugiat. Nullam sit amet massa metus. Duis in orci lectus. Praesent et erat nisl. Ut non eleifend odio. Fusce quis ultrices nisi. Suspendisse imperdiet lorem eu ligula finibus facilisis. Nullam sit amet orci viverra, iaculis ipsum sit amet, cursus nibh. Mauris semper, justo eget auctor vestibulum, sem erat bibendum turpis, at rhoncus mauris nisl vitae purus. Curabitur tempor mauris sit amet pulvinar faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi suscipit sed est ac finibus. Donec feugiat, felis eleifend congue posuere, ipsum libero fringilla ex, non blandit eros ipsum sit amet velit.',
+        announcement:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat tellus in iaculis feugiat. Nullam sit amet massa metus. Duis in orci lectus. Praesent et erat nisl. Ut non eleifend odio. Fusce quis ultrices nisi. Suspendisse imperdiet lorem eu ligula finibus facilisis. Nullam sit amet orci viverra, iaculis ipsum sit amet, cursus nibh. Mauris semper, justo eget auctor vestibulum, sem erat bibendum turpis, at rhoncus mauris nisl vitae purus. Curabitur tempor mauris sit amet pulvinar faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi suscipit sed est ac finibus. Donec feugiat, felis eleifend congue posuere, ipsum libero fringilla ex, non blandit eros ipsum sit amet velit.',
+        city: 'Barcelona',
+        sport: 'escalada',
+        members: [ids[0]],
+      },
+    ];
+    return Groups.create(groups);
+  })
+  .then((createdGroups) => {
+    console.log(`Created ${createdGroups.length} groups`);
   })
 
   .catch((error) => console.error(error));
+
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const User = require('../models/User.model');
 const { isLoggedOut } = require('../middleware');
+const fileUploader = require('../config/cloudinary.config')
 
 const saltRounds = 10;
 
@@ -10,28 +11,22 @@ const saltRounds = 10;
 const express = require('express');
 const router = express.Router(); //Lo importamos de express, vamos a crear una instancia de un Router, de  un elemento que nos permite gestionar las rutas de la aplicación
 
-// router.get("/", (req, res) => {
-//     res.render('index'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
-// })
 
-router.get('/signup', (req, res) => {
-  res.render('signup'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
+router.get('/signup', isLoggedOut,(req, res) => {
+  res.render('/signup'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
 });
 
-router.get('/login', (req, res) => {
-  res.render('login'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
-});
-
-router.get('/whoRwe', (req, res) => {
-  res.render('whoRwe'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
-});
-
-router.get('/signup', isLoggedOut, (req, res) => {
-  res.render('signup');
-});
-
-router.post('/signup', (req, res) => {
-  const { username, password } = req.body;
+router.post('/signup', fileUploader.single('image'), (req, res) => {
+  const {
+    username,
+    password,
+    email,
+    age,
+    city,
+    genero,
+    sports,
+    description,
+  } = req.body;
 
   if (!username || !password) {
     res.render('signup', {
@@ -39,24 +34,33 @@ router.post('/signup', (req, res) => {
     });
   }
 
-  // const regularExpresion = new RegExp('');
-  // regularExpresion.test(password)
-
   if (password.length < 3) {
     res.render('signup', {
       errorMessage: 'Password should have at least 3 characters',
     });
   }
 
-  User.findOne({ username }).then((user) => {
+  User.findOne({ username })
+    .then((user) => {
     if (user) {
       return res.render('signup', { errorMessage: 'User already exists.' });
     }
 
     const salt = bcrypt.genSaltSync(saltRounds);
     const hashPass = bcrypt.hashSync(password, salt);
+    const picture = req.file.path;
 
-    User.create({ username, password: hashPass })
+    User.create({
+      username,
+      password: hashPass,
+      email,
+      age,
+      city,
+      genero,
+      sports,
+      description,
+      picture,
+    })
       .then((newUser) => {
         // return res.redirect('/');
         req.login(newUser, (error) => {
@@ -76,17 +80,19 @@ router.post('/signup', (req, res) => {
 });
 
 router.get('/login', isLoggedOut, (req, res) => {
-  res.render('login');
+  res.render('login'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
 });
 
-router.post(
-  '/login',
-  passport.authenticate('local', {
+router.post('/login', passport.authenticate('local', {
     successRedirect: '/private/profile',
     failureRedirect: '/login',
     passReqToCallback: true,
   })
 );
+
+router.get('/whoRwe', (req, res) => {
+  res.render('whoRwe'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
+});
 
 router.get('/logout', (req, res) => {
   req.logout();

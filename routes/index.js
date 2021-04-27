cbb7b2634b427f1cb81e6c99c388dae670ff9db4
triana <trianaheinz@gmail.com>
2021-04-27 12:55:19 +0200

    sign up updated

--- routes/index.js
+++ routes/index.js
@@ -11,96 +11,128 @@ const express = require('express');
 const router = express.Router(); //Lo importamos de express, vamos a crear una instancia de un Router, de  un elemento que nos permite gestionar las rutas de la aplicación
 
 router.get('/', isLoggedOut, (req, res) => {
-    res.render('index'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
+  res.render('index'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
 });
 
 router.get('/signup', isLoggedOut, (req, res) => {
-    res.render('signup'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
+  res.render('signup'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
 });
 
 router.post('/signup', fileUploader.single('image'), (req, res) => {
-    const {
-        username,
-        password,
-        email,
-        age,
-        city,
-        genero,
-        sports,
-        description,
-    } = req.body;
+  const {
+    username,
+    password,
+    email,
+    age,
+    city,
+    genero,
+    sports,
+    description,
+  } = req.body;
 
-    if (!username || !password) {
-        res.render('signup', {
-            errorMessage: 'Username and password are required.',
-        });
+  if (!username || !password) {
+    res.render('signup', {
+      errorMessage: 'Username and password are required.',
+    });
+  }
+
+  if (password.length < 3) {
+    res.render('signup', {
+      errorMessage: 'Password should have at least 3 characters',
+    });
+  }
+  console.log('dataBase.findOne');
+  User.findOne({ username }).then((user) => {
+    console.log('insideFinOne');
+    if (user) {
+      return res.render('signup', { errorMessage: 'User already exists.' });
     }
 
-    if (password.length < 3) {
-        res.render('signup', {
-            errorMessage: 'Password should have at least 3 characters',
-        });
-    }
-    console.log('dataBase.findOne');
-    User.findOne({ username })
-        .then((user) => {
-            console.log('insideFinOne');
-            if (user) {
-                return res.render('signup', { errorMessage: 'User already exists.' });
-            }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashPass = bcrypt.hashSync(password, salt);
+    const picture = req.file.path;
 
-            const salt = bcrypt.genSaltSync(saltRounds);
-            const hashPass = bcrypt.hashSync(password, salt);
-            const picture = req.file.path;
-
-            User.create({
-                    username,
-                    password: hashPass,
-                    email,
-                    age,
-                    city,
-                    genero,
-                    sports,
-                    description,
-                    picture,
-                })
-                .then((newUser) => {
-                    console.log('user created');
-                    req.login(newUser, (error) => {
-                        if (error) {
-                            next(error);
-                        }
-                        return res.redirect('/private/profile');
-                    });
-                })
-                .catch((error) => {
-                    console.log(error);
-                    return res.render('signup', {
-                        errorMessage: 'Server error. Try again.',
-                    });
-                });
+    User.create({
+      username,
+      password: hashPass,
+      email,
+      age,
+      city,
+      genero,
+      sports,
+      description,
+      picture,
+    })
+      .then((newUser) => {
+        console.log('user created');
+        req.login(newUser, (error) => {
+          if (error) {
+            next(error);
+          }
+          return res.redirect('/private/profile');
         });
+      })
+      .catch((error) => {
+        console.log(error);
+        return res.render('signup', {
+          errorMessage: 'Server error. Try again.',
+        });
+      });
+  });
 });
 
 router.get('/login', isLoggedOut, (req, res) => {
-    res.render('login'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
+  res.render('login'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
 });
 
-router.post('/login', passport.authenticate('local', {
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === '' || password === '') {
+    res.render('login', {
+      errorMessage: 'Please enter username and password',
+    });
+    return;
+  }
+  console.log("email: ", email)
+  console.log('password: ', password);
+  User.findOne({ email })
+    .then((user) => {
+      console.log("user: ", user)
+      if (!user) {
+        res.render('login', {
+          errorMessage: 'User not found',
+        });
+
+        return;
+      }
+      const passwordCorrect = crypt.compareSync(password, user.password);
+
+      if (passwordCorrect) {
+        console.log(req.session);
+
+        req.session.currentUser = user; // Triggers creation of the session and cookie
+        res.redirect('/');
+      } else {
+        res.render('login', {
+          errorMessage: 'Incorrect email or password',
+        });
+      }
+    })
+    .catch((err) => next(err));
+  /* passport.authenticate('local', {
     successRedirect: '/private/profile',
     failureRedirect: '/login',
     passReqToCallback: true,
-}));
+}) */
+});
 
 router.get('/whoRwe', (req, res) => {
-    res.render('whoRwe'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
+  res.render('whoRwe'); //Renderizara la ruta index, si escribes / puedes poner cualquier nombre, pero sera tu pagina principal.
 });
 
 router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
-
-
 
 module.exports = router; //exportar el router, lo debemos importar a app.js
